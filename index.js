61da853b1b371a1af89c9ceb2a1d626b54763375
aisha-bhudye <bhudyeaisha@gmail.com>
2025-10-19 11:28:52 +0100

    fixed index.js

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ const port = 8000;
 const mainRoutes = require("./routes/main");  
 app.use('/', mainRoutes);
 
-// Handle the routes
-app.get("/", (req, res) => res.send("Hello World!")); 
-app.get('/about', (req, res) => res.send ('<h1>This is about page</h1>'))//This takes you to the about page
-app.get('/contact', (req, res) => res.send ('<h1>aisha.bhudye@gmail.com</h1>'))//this takes you to the contact page
-app.get('/date', (req, res) => res.send ('<h1>19/10/2025</h1>'))//this takes you to the date page
-
 // Start listening for HTTP requests
 app.listen(port, 
     () => console.log(`Node server is running on port ${port}...`)); 

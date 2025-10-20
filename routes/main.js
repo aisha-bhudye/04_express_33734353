6cf4c3a65a921032c5dbeff40eeead36c02cbc64
aisha-bhudye <bhudyeaisha@gmail.com>
2025-10-20 17:13:54 +0100

    date time fix

--- routes/main.js
+++ routes/main.js
@@ -8,7 +8,10 @@ router.get("/", (req, res) => res.send("Hello World!"));
 
 router.get("/about", (req, res) => res.send ("<h1>This is the about page</h1>"));
 router.get('/contact', (req, res) => res.send ('<h1>aisha.bhudye@gmail.com</h1>'))//this takes you to the contact page
-router.get('/date', (req, res) => res.send ('<h1>19/10/2025</h1>'))//this takes you to the date page
+router.get("/date", (req, res) => {
+	var date = new Date(); //get the date and time currently 
+	res.send("<h1>Current Date and Time:</h1><p>" + date + "</p>");
+})
 router.get('/Ronald', (req, res) => res.send ('<h1>Welcome Ronald</h1>'))//Ronald 
 router.get('/Jemima', (req, res) => res.send ('<h1>Welcome Jemima</h1>'))//Jemima 
 
